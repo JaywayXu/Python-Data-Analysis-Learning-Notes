14fd285714e2f99fac04f6097287d7e9484176fb
Cloud_Xudell <1786546913@qq.com>
2020-04-08 14:22:11 +0800

    update

--- Matplotlib/demo_00Mofandemo/19_animation_demo.py
+++ Matplotlib/demo_00Mofandemo/19_animation_demo.py
@@ -3,11 +3,12 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 
 fig, ax = plt.subplots()
-x = np.arange(0, 2*np.pi, 0.01) # 表示从0~2*np.pi之间每隔0.01取一个点
+x = np.arange(0, 2*np.pi, 0.01)  # 表示从0~2*np.pi之间每隔0.01取一个点
 line, = ax.plot(x, np.sin(x))
 
 
-def animate(i):
+def animate(i):  # 这里的i其实就是参数
+    print(i)
     line.set_ydata(np.sin(x + i/10.0))
     return line,
 
@@ -20,6 +21,6 @@ def init():
 ani = animation.FuncAnimation(fig=fig, func=animate, frames=100, init_func=init,
                               interval=20, blit=False)
 """frames设定帧数,总共执行100个update就会进行下一次循环，并且frames还会作为参数传入animate()函数，init_func设定初始函数图像, interval设置更新间隔此处设置为20毫秒
-如果是变化了的就更新设置为True,如果是全部更新的话就设置为False
+blit如果是只有变化了的像素点才更新就设置为True,如果是整张图片所有像素点全部更新的话就设置为False
 """
 plt.show()
---
+++ Matplotlib/demo_00Mofandemo/animation_simpleani.py
@@ -0,0 +1,35 @@
+"""
+==================
+Animated line plot
+==================
+"""
+# https://github.com/matplotlib/matplotlib/blob/master/examples/animation/simple_anim.py
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+x = np.arange(0, 2*np.pi, 0.01)
+line, = ax.plot(x, np.sin(x))
+
+
+def animate(i):
+    line.set_ydata(np.sin(x + i / 100))  # update the data.
+    return line,
+
+
+ani = animation.FuncAnimation(
+    fig, animate, interval=2, blit=True, save_count=50)
+
+# To save the animation, use e.g.
+#
+# ani.save("movie.mp4")
+#
+# or
+#
+# writer = animation.FFMpegWriter(
+#     fps=15, metadata=dict(artist='Me'), bitrate=1800)
+# ani.save("movie.mp4", writer=writer)
+
+plt.show()
